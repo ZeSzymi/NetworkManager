ac62aafa3ce8f91b23bcdd696f1c84ca18fc579d
Afur <teodorowiczartur@gmail.com>
2020-01-08 19:09:07 +0100

    Add project files.

---
+++ NetworkManager/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Prism.Windows.Mvvm;
+
+namespace NetworkManager.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ NetworkManager/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using NetworkManager.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace NetworkManager.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel => DataContext as MainViewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
